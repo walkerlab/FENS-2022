b0c35cc4bb8beebb9dcc2383f0286d27a0feefad
Edgar Y. Walker <edgar.walker@gmail.com>
2022-05-19 04:39:01 -0500

    Clean out unused code

--- fens/utility/measures.py
+++ fens/utility/measures.py
@@ -74,9 +74,6 @@ def model_predictions(model, dataloader, device="cpu"):
 
     target, output = torch.empty(0), torch.empty(0)
     for images, responses in dataloader:
-        if len(images.shape) == 5:
-            images = images.squeeze(dim=0)
-            responses = responses.squeeze(dim=0)
         with torch.no_grad():
             with device_state(model, device) if not isinstance(
                 model, types.FunctionType
@@ -118,6 +115,9 @@ def get_avg_correlations(model, dataloader, device="cpu", per_neuron=True, **kwa
 
 
 def get_correlations(model, dataloader, device="cpu", per_neuron=True, **kwargs):
+    """
+    Returns correlation between model outputs and responses
+    """
     with eval_state(model) if not isinstance(
         model, types.FunctionType
     ) else contextlib.nullcontext():
@@ -241,171 +241,3 @@ def compute_oracle_corr(repeated_outputs):
 
             oracles.append(oracle)
         return corr(np.vstack(repeated_outputs), np.vstack(oracles), axis=0)
-
-
-# def get_fraction_oracles(model, dataloaders, device="cpu", corrected=False):
-#     dataloaders = dataloaders["test"] if "test" in dataloaders else dataloaders
-#     if corrected:
-#         oracles = get_oracles_corrected(dataloaders=dataloaders, as_dict=False, per_neuron=True)
-#     else:
-#         oracles = get_oracles(dataloaders=dataloaders, as_dict=False, per_neuron=True)
-#     test_correlation = get_correlations(
-#         model=model, dataloaders=dataloaders, device=device, as_dict=False, per_neuron=True
-#     )
-#     oracle_performance, _, _, _ = np.linalg.lstsq(np.hstack(oracles)[:, np.newaxis], np.hstack(test_correlation))
-#     return oracle_performance
-
-
-# def get_explainable_var(dataloaders, as_dict=False, per_neuron=True):
-#     dataloaders = dataloaders["test"] if "test" in dataloaders else dataloaders
-#     explainable_var = {}
-#     for k, v in dataloaders.items():
-#         _, outputs = get_repeats(v)
-#         explainable_var[k] = compute_explainable_var(outputs)
-#     if not as_dict:
-#         explainable_var = (
-#             np.hstack([v for v in explainable_var.values()])
-#             if per_neuron
-#             else np.mean(np.hstack([v for v in explainable_var.values()]))
-#         )
-#     return explainable_var
-
-
-# def compute_explainable_var(outputs, eps=1e-9):
-#     ImgVariance = []
-#     TotalVar = np.var(np.vstack(outputs), axis=0, ddof=1)
-#     for out in outputs:
-#         ImgVariance.append(np.var(out, axis=0, ddof=1))
-#     ImgVariance = np.vstack(ImgVariance)
-#     NoiseVar = np.mean(ImgVariance, axis=0)
-#     explainable_var = (TotalVar - NoiseVar) / (TotalVar + eps)
-#     return explainable_var
-
-
-# def get_FEV(model, dataloaders, device="cpu", as_dict=False, per_neuron=True, threshold=None):
-#     """
-#     Computes the fraction of explainable variance explained (FEVe) per Neuron, given a model and a dictionary of dataloaders.
-#     The dataloaders will have to return batches of identical images, with the corresponing neuronal responses.
-#     Args:
-#         model (object): PyTorch module
-#         dataloaders (dict): Dictionary of dataloaders, with keys corresponding to "data_keys" in the model
-#         device (str): 'cuda' or 'gpu
-#         as_dict (bool): Returns the scores as a dictionary ('data_keys': values) if set to True.
-#         per_neuron (bool): Returns the grand average if set to True.
-#         threshold (float): for the avg feve, excludes neurons with a explainable variance below threshold
-#     Returns:
-#         FEV (dict, or np.array, or float): Fraction of explainable varianced explained. Per Neuron or as grand average.
-#     """
-#     dataloaders = dataloaders["test"] if "test" in dataloaders else dataloaders
-#     FEV = {}
-#     with eval_state(model) if not isinstance(model, types.FunctionType) else contextlib.nullcontext():
-#         for data_key, dataloader in dataloaders.items():
-#             targets, outputs = model_predictions_repeats(
-#                 model=model, dataloader=dataloader, data_key=data_key, device=device, broadcast_to_target=True
-#             )
-#             if threshold is None:
-#                 FEV[data_key] = compute_FEV(targets=targets, outputs=outputs)
-#             else:
-#                 fev, feve = compute_FEV(targets=targets, outputs=outputs, return_exp_var=True)
-#                 FEV[data_key] = feve[fev > threshold]
-#     if not as_dict:
-#         FEV = np.hstack([v for v in FEV.values()]) if per_neuron else np.mean(np.hstack([v for v in FEV.values()]))
-#     return FEV
-
-
-# def compute_FEV(targets, outputs, return_exp_var=False):
-#     """
-#     Args:
-#         targets (list): Neuronal responses (ground truth) to image repeats. Dimensions: [num_images] np.array(num_reaps, num_neurons)
-#         outputs (list): Model predictions to the repeated images, with an identical shape as the targets
-#         return_exp_var (bool): returns the fraction of explainable variance per neuron if set to True
-#     Returns:
-#         FEVe (np.array): the fraction of explainable variance explained per neuron
-#         --- optional: FEV (np.array): the fraction
-#     """
-#     ImgVariance = []
-#     PredVariance = []
-#     for i, _ in enumerate(targets):
-#         PredVariance.append((targets[i] - outputs[i]) ** 2)
-#         ImgVariance.append(np.var(targets[i], axis=0, ddof=1))
-#     PredVariance = np.vstack(PredVariance)
-#     ImgVariance = np.vstack(ImgVariance)
-
-#     TotalVar = np.var(np.vstack(targets), axis=0, ddof=1)
-#     NoiseVar = np.mean(ImgVariance, axis=0)
-#     FEV = (TotalVar - NoiseVar) / TotalVar
-
-#     PredVar = np.mean(PredVariance, axis=0)
-#     FEVe = 1 - (PredVar - NoiseVar) / (TotalVar - NoiseVar)
-#     return [FEV, FEVe] if return_exp_var else FEVe
-
-
-# def get_cross_oracles(data, reference_data):
-#     _, outputs = get_repeats(data)
-#     _, outputs_reference = get_repeats(reference_data)
-#     cross_oracles = compute_cross_oracles(outputs, outputs_reference)
-#     return cross_oracles
-
-
-# def compute_cross_oracles(repeats, reference_data):
-#     pass
-
-
-# def normalize_RGB_channelwise(mei):
-#     mei_copy = mei.copy()
-#     mei_copy = mei_copy - mei_copy.min(axis=(1, 2), keepdims=True)
-#     mei_copy = mei_copy / mei_copy.max(axis=(1, 2), keepdims=True)
-#     return mei_copy
-
-
-# def normalize_RGB(mei):
-#     mei_copy = mei.copy()
-#     mei_copy = mei_copy - mei_copy.min()
-#     mei_copy = mei_copy / mei_copy.max()
-#     return mei_copy
-
-
-# def get_model_rf_size(model_config):
-#     layers = model_config["layers"]
-#     input_kern = model_config["input_kern"]
-#     hidden_kern = model_config["hidden_kern"]
-#     dil = model_config["hidden_dilation"]
-#     rf_size = input_kern + ((hidden_kern - 1) * dil) * (layers - 1)
-#     return rf_size
-
-
-# def get_predictions(model, dataloaders, device="cpu", as_dict=False, per_neuron=True, test_data=True, **kwargs):
-#     predictions = {}
-#     with eval_state(model) if not isinstance(model, types.FunctionType) else contextlib.nullcontext():
-#         for k, v in dataloaders.items():
-#             if test_data:
-#                 _, output = model_predictions_repeats(dataloader=v, model=model, data_key=k, device=device)
-#             else:
-#                 _, output = model_predictions(dataloader=v, model=model, data_key=k, device=device)
-#             predictions[k] = output.T
-
-#     if not as_dict:
-#         predictions = [v for v in predictions.values()]
-#     return predictions
-
-
-# def get_targets(model, dataloaders, device="cpu", as_dict=True, per_neuron=True, test_data=True, **kwargs):
-#     responses = {}
-#     with eval_state(model) if not isinstance(model, types.FunctionType) else contextlib.nullcontext():
-#         for k, v in dataloaders.items():
-#             if test_data:
-#                 targets, _ = model_predictions_repeats(dataloader=v, model=model, data_key=k, device=device)
-#                 targets_per_neuron = []
-#                 for i in range(targets[0].shape[1]):
-#                     neuronal_responses = []
-#                     for repeats in targets:
-#                         neuronal_responses.append(repeats[:, i])
-#                     targets_per_neuron.append(neuronal_responses)
-#                 responses[k] = targets_per_neuron
-#             else:
-#                 targets, _ = model_predictions(dataloader=v, model=model, data_key=k, device=device)
-#                 responses[k] = targets.T
-
-#     if not as_dict:
-#         responses = [v for v in responses.values()]
-#     return responses
